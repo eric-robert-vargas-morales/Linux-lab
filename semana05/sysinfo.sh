a207c291f654270cd143fa85d77a75452c9045df
Eric Robert Vargas Morales <eric69904@gmail.com>
2026-03-14 17:33:28 -0400

    feat: add CPU and memory sections

--- semana05/sysinfo.sh
+++ semana05/sysinfo.sh
@@ -59,8 +59,35 @@ seccion_general() {
 	printf " %-18s %s\n" "Sistema:" 	"$(uname -s)"
 	printf " %-18s %s\n" "Kernel:" 		"$(uname -r)"
 	printf " %-18s %s\n" "Arquitectura:" 	"$(uname -m)"
-	printf " %-18s %s\n" "Fecha/Hora:" 	"$(date '+%d/%m/%Y%H:%M:%S')"
+	printf " %-18s %s\n" "Fecha/Hora:" 	"$(date '+%d/%m/%Y %H:%M:%S')"
 	printf " %-18s %s\n" "Encendido:" 	"$(uptime -p)"
+	echo ""
+}
+# === seccion 2: CPU ===
+seccion_cpu() {
+	echo "[ CPU ]"
+	echo "$SEPARADOR_SEC"
+	nucleos=$(nproc)
+	carga=$(uptime | awk -F'load average:' '{print $2}' | awk '{print $1}' | tr -d ',')
+	printf "   %-18s %s\n" "Nucleos:"	"$nucleos"
+	printf "   %-18s %s\n" "Carga (1min):"		"$carga"
+	echo ""
+}
+# === seccion 3: Memoria RAM ===
+seccion_memoria() {
+	echo "[ MEMORIA RAM ]"
+	echo "$SEPARADOR_SEC"
+	mem_total=$(free -h | awk '/^Mem:/ {print $2}')
+	mem_usado=$(free -h | awk '/^Mem:/ {print $3}')
+	mem_libre=$(free -h | awk '/^Mem:/ {print $4}')
+	swap_total=$(free -h | awk '/^Swap:/ {print $2}')
+	swap_usado=$(free -h | awk '/^Swap:/ {print $3}')
+
+	printf "  %-18s %s\n" "RAM total:" 	"$mem_total"
+	printf "  %-18s %s\n" "RAM usada:" 	"$mem_usado"
+	printf "  %-18s %s\n" "RAM libre:" 	"$mem_libre"
+	printf "  %-18s %s\n" "Swap total:" 	"$swap_total"
+	printf "  %-18s %s\n" "Swap usado:" 	"$swap_usado"
 	echo ""
 }
 
@@ -69,6 +96,12 @@ printf "	REPORTE DEL SISTEMA	-	sysinfo.sh v%s\n" "$VERSION"
 echo "$SEPARADOR"
 echo ""
 # === Ejecutar segun el modo ===
-if [ "$MODO" = "all" ]; then
-	seccion_general
-fi
+case "$MODO" in
+	all)
+		seccion_general
+		seccion_cpu
+		seccion_memoria
+		;;
+	cpu)	seccion_cpu	;;
+	mem)	seccion_memoria	;;
+esac
